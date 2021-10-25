2deeaa0e0b2ac218853428fdaab15facbdc2810d
Matt Pieklik <mp@vectra.ai>
2021-10-25 16:31:25 -0500

    Deprecating script

--- cybereason/config.py
+++
@@ -1,9 +0,0 @@
-
-# Cybereason config
-SERVER = '<instance name>.cybereason.net'
-PORT = '443'
-
-# Vectra config
-COGNITO_BRAIN = 'https://<cognito.example.com>'
-COGNITO_TOKEN = '<cognito detect api token>'
-
